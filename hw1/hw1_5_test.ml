a4366db548192b88926433c5e8130333aff014a6
Ik Hwan Kim <12kimih@snu.ac.kr>
2019-09-12 04:20:09 +0900

    Modified hw1-1 & New algorithm for hw1-5,6

--- hw1/hw1_5_test.ml
+++ hw1/hw1_5_test.ml
@@ -4,23 +4,48 @@ type crazy2 =
   | ONE of crazy2
   | MONE of crazy2
 
+(*
 let rec crazy2val n =
   let rec len n =
     match n with
     | NIL -> 0
-    | ZERO k -> 1 + len k
-    | ONE k -> 1 + len k
-    | MONE k -> 1 + len k
+    | ZERO tl -> 1 + len tl
+    | ONE tl -> 1 + len tl
+    | MONE tl -> 1 + len tl
   in
   let rec power_of_2 n =
     if n > 0 then 2 * power_of_2 (n - 1) else 1
   in
   match n with
   | NIL -> 0
-  | ZERO k -> crazy2val k
-  | ONE k -> crazy2val k + power_of_2 (len k)
-  | MONE k -> crazy2val k - power_of_2 (len k)
+  | ZERO tl -> crazy2val tl
+  | ONE tl -> crazy2val tl + power_of_2 (len tl)
+  | MONE tl -> crazy2val tl - power_of_2 (len tl)
+*)
+
+let rec append_crazy2 n1 n2 =
+  match n1 with
+  | NIL -> n2
+  | ZERO n1_tl -> ZERO (append_crazy2 n1_tl n2)
+  | ONE n1_tl -> ONE (append_crazy2 n1_tl n2)
+  | MONE n1_tl -> MONE (append_crazy2 n1_tl n2)
+
+let rec reverse_crazy2 n =
+  match n with
+  | NIL -> NIL
+  | ZERO tl -> append_crazy2 (reverse_crazy2 tl) (ZERO NIL)
+  | ONE tl -> append_crazy2 (reverse_crazy2 tl) (ONE NIL)
+  | MONE tl -> append_crazy2 (reverse_crazy2 tl) (MONE NIL)
+
+let crazy2val n =
+  let rec crazy2val_rev n =
+    match n with
+    | NIL -> 0
+    | ZERO tl -> 2 * crazy2val_rev tl
+    | ONE tl -> 2 * crazy2val_rev tl + 1
+    | MONE tl -> 2 * crazy2val_rev tl - 1
+  in crazy2val_rev (reverse_crazy2 n)
 
 (* test *)
-let test_c = MONE(MONE(MONE NIL))
+let test_c = MONE(ONE(ZERO NIL))
 let () = print_endline (string_of_int (crazy2val test_c))
--- hw1/hw1_6_test.ml
+++ hw1/hw1_6_test.ml
@@ -4,41 +4,72 @@ type crazy2 =
   | ONE of crazy2
   | MONE of crazy2
 
-let rec crazy2val n =
-  let rec len n =
-    match n with
-    | NIL -> 0
-    | ZERO k -> 1 + len k
-    | ONE k -> 1 + len k
-    | MONE k -> 1 + len k
-  in
-  let rec power_of_2 n =
-    if n > 0 then 2 * power_of_2 (n - 1) else 1
-  in
+let rec append_crazy2 n1 n2 =
+  match n1 with
+  | NIL -> n2
+  | ZERO n1_tl -> ZERO (append_crazy2 n1_tl n2)
+  | ONE n1_tl -> ONE (append_crazy2 n1_tl n2)
+  | MONE n1_tl -> MONE (append_crazy2 n1_tl n2)
+
+let rec reverse_crazy2 n =
   match n with
-  | NIL -> 0
-  | ZERO k -> crazy2val k
-  | ONE k -> crazy2val k + power_of_2 (len k)
-  | MONE k -> crazy2val k - power_of_2 (len k)
+  | NIL -> NIL
+  | ZERO tl -> append_crazy2 (reverse_crazy2 tl) (ZERO NIL)
+  | ONE tl -> append_crazy2 (reverse_crazy2 tl) (ONE NIL)
+  | MONE tl -> append_crazy2 (reverse_crazy2 tl) (MONE NIL)
+
+let rec trim_crazy2 n =
+  match n with
+  | NIL -> ZERO NIL
+  | ZERO tl -> trim_crazy2 tl
+  | ONE tl -> n
+  | MONE tl -> n
 
 let crazy2add n1 n2 =
-  let rec append_crazy2 n1 n2 =
+  let rec crazy2add_rev n1 n2 =
     match n1 with
     | NIL -> n2
-    | ZERO n1_tl -> ZERO (append_crazy2 n1_tl n2)
-    | ONE n1_tl -> ONE (append_crazy2 n1_tl n2)
-    | MONE n1_tl -> MONE (append_crazy2 n1_tl n2)
-  in
-  let rec crazy2repr n =
-    if n = 0 then ZERO NIL else
-    if (n mod 2) = 1 then append_crazy2 (crazy2repr (n / 2)) (ONE NIL) else
-    if (n mod 2) = 0 then append_crazy2 (crazy2repr (n / 2)) (ZERO NIL) else append_crazy2 (crazy2repr (n / 2)) (MONE NIL)
-  in
-  let sum = crazy2val n1 + crazy2val n2 in crazy2repr sum
+    | ZERO tl1 ->
+      (match n2 with
+      | NIL -> n1
+      | ZERO tl2 -> append_crazy2 (ZERO NIL) (crazy2add_rev tl1 tl2) 
+      | ONE tl2 -> append_crazy2 (ONE NIL) (crazy2add_rev tl1 tl2)
+      | MONE tl2 -> append_crazy2 (MONE NIL) (crazy2add_rev tl1 tl2))
+    | ONE tl1 ->
+      (match n2 with
+      | NIL -> n1
+      | ZERO tl2 -> append_crazy2 (ONE NIL) (crazy2add_rev tl1 tl2)
+      | ONE tl2 -> append_crazy2 (ZERO NIL) (crazy2add_rev (ONE NIL) (crazy2add_rev tl1 tl2))
+      | MONE tl2 -> append_crazy2 (ZERO NIL) (crazy2add_rev tl1 tl2))
+    | MONE tl1 ->
+      (match n2 with
+      | NIL -> n1
+      | ZERO tl2 -> append_crazy2 (MONE NIL) (crazy2add_rev tl1 tl2)
+      | ONE tl2 -> append_crazy2 (ZERO NIL) (crazy2add_rev tl1 tl2)
+      | MONE tl2 -> append_crazy2 (ZERO NIL) (crazy2add_rev (MONE NIL) (crazy2add_rev tl1 tl2)))
+  in trim_crazy2 (reverse_crazy2 (crazy2add_rev (reverse_crazy2 n1) (reverse_crazy2 n2)))
+
+let crazy2val n =
+  let rec crazy2val_rev n =
+    match n with
+    | NIL -> 0
+    | ZERO tl -> 2 * crazy2val_rev tl
+    | ONE tl -> 2 * crazy2val_rev tl + 1
+    | MONE tl -> 2 * crazy2val_rev tl - 1
+  in crazy2val_rev (reverse_crazy2 n)
+
+(*
+let rec crazy2repr n =
+  if n = 0 then NIL else
+  if (n mod 2) = 1 then append_crazy2 (crazy2repr (n / 2)) (ONE NIL) else
+  if (n mod 2) = 0 then append_crazy2 (crazy2repr (n / 2)) (ZERO NIL) else append_crazy2 (crazy2repr (n / 2)) (MONE NIL)
+
+let crazy2add n1 n2 = crazy2repr (crazy2val n1 + crazy2val n2)
+*)
 
 (* test *)
-let n1 = ONE(MONE(ZERO NIL))
-let n2 = MONE(MONE(ONE NIL))
+let n1 = MONE(MONE(ZERO NIL))
+let n2 = ONE(MONE(ONE NIL))
 
 let rec crazy2show n =
   match n with
@@ -49,3 +80,4 @@ let rec crazy2show n =
 
 let () = print_endline (crazy2show (crazy2add n1 n2))
 let () = print_endline (string_of_int (crazy2val (crazy2add n1 n2)))
+let () = print_endline ("Expected value: " ^ string_of_int (crazy2val n1 + crazy2val n2))
