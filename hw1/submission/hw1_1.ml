83667c21f889526ce39857faf7e9d6598b62f793
Ik Hwan Kim <12kimih@snu.ac.kr>
2019-09-12 18:55:45 +0900

    Shorten code for hw1-1

--- hw1/submission/hw1_1.ml
+++ hw1/submission/hw1_1.ml
@@ -1,3 +1,2 @@
 let rec sigma (a, b, f) =
-  if a < b then f a + sigma (a + 1, b, f) else
-  if a = b then f a else 0
+  if a <= b then f a + sigma (a + 1, b, f) else 0
--- hw1/test/hw1_1_test.ml
+++ hw1/test/hw1_1_test.ml
@@ -1,9 +1,8 @@
 let rec sigma (a, b, f) =
-  if a < b then f a + sigma (a + 1, b, f) else
-  if a = b then f a else 0
+  if a <= b then f a + sigma (a + 1, b, f) else 0
 
 (* test *)
-let a = 10
+let a = 8
 let b = 10
 let f x = x
 let () = print_endline (string_of_int (sigma (a, b, f)))
