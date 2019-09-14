738f772c8d46c22f9b1c76e268b5810e5b03fa0f
Ik Hwan Kim <12kimih@snu.ac.kr>
2019-09-14 09:59:36 +0900

    Test files for hw2-1,2

---
+++ hw2/test/hw2_1_test.ml
@@ -0,0 +1,57 @@
+type exp =
+  | X
+  | INT of int
+  | REAL of float
+  | ADD of exp * exp
+  | SUB of exp * exp
+  | MUL of exp * exp
+  | DIV of exp * exp
+  | SIGMA of exp * exp * exp
+  | INTEGRAL of exp * exp * exp
+
+exception FreeVariable
+
+let rec calculate e =
+  let rec substitute e n =
+    match e with
+    | X -> n
+    | INT i -> float_of_int i
+    | REAL f -> f
+    | ADD (e1, e2) -> substitute e1 n +. substitute e2 n
+    | SUB (e1, e2) -> substitute e1 n -. substitute e2 n
+    | MUL (e1, e2) -> substitute e1 n *. substitute e2 n
+    | DIV (e1, e2) -> substitute e1 n /. substitute e2 n
+    | SIGMA (e1, e2, e3) ->
+      let e1_val = int_of_float (calculate e1) in
+      let e2_val = int_of_float (calculate e2) in
+      if e1_val <= e2_val then substitute e3 (float_of_int e1_val) +. substitute (SIGMA (INT (e1_val + 1), INT e2_val, e3)) n else 0.
+    | INTEGRAL (e1, e2, e3) ->
+      let e1_val = calculate e1 in
+      let e2_val = calculate e2 in
+      if e1_val > e2_val +. 0.1 then ~-.(substitute e n) else
+      if e1_val < e2_val -. 0.1 then substitute e3 e1_val *. 0.1 +. substitute (INTEGRAL (REAL (e1_val +. 0.1), REAL e2_val, e3)) n else 0.
+  in
+  match e with
+  | X -> raise(FreeVariable)
+  | INT i -> float_of_int i
+  | REAL f -> f
+  | ADD (e1, e2) -> calculate(e1) +. calculate(e2)
+  | SUB (e1, e2) -> calculate(e1) -. calculate(e2)
+  | MUL (e1, e2) -> calculate(e1) *. calculate(e2)
+  | DIV (e1, e2) -> calculate(e1) /. calculate(e2)
+  | SIGMA (e1, e2, e3) ->
+    let e1_val = int_of_float (calculate e1) in
+    let e2_val = int_of_float (calculate e2) in
+    if e1_val <= e2_val then substitute e3 (float_of_int e1_val) +. calculate (SIGMA (INT (e1_val + 1), INT e2_val, e3)) else 0.
+  | INTEGRAL (e1, e2, e3) ->
+    let e1_val = calculate e1 in
+    let e2_val = calculate e2 in
+    if e1_val > e2_val +. 0.1 then ~-.(calculate e) else
+    if e1_val < e2_val -. 0.1 then substitute e3 e1_val *. 0.1 +. calculate (INTEGRAL (REAL (e1_val +. 0.1), REAL e2_val, e3)) else 0.
+
+let a = SIGMA(INT 1, INT 10, SUB(MUL(X, X), INT 1))
+let b = INTEGRAL(REAL 1.0, REAL 10.0, SUB(MUL(X, X), INT 1))
+let () = print_float (calculate a)
+let () = print_endline ""
+let () = print_float (calculate b)
+let () = print_endline ""
---
+++ hw2/test/hw2_2_test.ml
@@ -0,0 +1,22 @@
+type ae =
+  | CONST of int
+  | VAR of string
+  | POWER of string * int
+  | TIMES of ae list
+  | SUM of ae list
+
+let rec diff (e, var) =
+  match e with
+  | CONST i -> CONST 0
+  | VAR e_var -> if e_var = var then CONST 1 else CONST 0
+  | POWER (e_var, i) ->
+    if e_var <> var then CONST 0 else
+    if i = 0 then CONST 0 else TIMES [CONST i; POWER (e_var, i - 1)]
+  | TIMES ae_list ->
+    (match ae_list with
+    | [] -> CONST 0
+    | hd :: tl -> SUM [TIMES [diff (hd, var); TIMES tl]; TIMES[hd; diff (TIMES tl, var)]])
+  | SUM ae_list ->
+    (match ae_list with
+    | [] -> CONST 0
+    | hd :: tl -> SUM [diff (hd, var); diff (SUM tl, var)])
